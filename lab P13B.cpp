dbd0281e9d28c5147184728b0017f8e71be74141
Breanna <b-sproul@onu.edu>
2023-11-30 13:44:07 -0800

    Completed P13B-2

--- lab P13B.cpp
+++ lab P13B.cpp
@@ -5,27 +5,52 @@ using namespace std;
 
 //function prototypes
 bool isLeapYear(int year);
+int daysInMonth(int month, int year);
+
 
 int main() {
-	cout << "Enter a year or Q to quit: ";
+	cout << "Enter a month and year or Q to quit: ";
 	int year;
-	cin >> year;
-	if (year == 0) {
+	int month;
+	cin >> month >> year;
+	if (month == 0) {
 		return 0;
 	}
 
-
+	int days = daysInMonth(month, year);
 	
 	if (isLeapYear(year) == true) {
-		cout << year << " is a leap year.";
+		//cout << year << " is a leap year.";
+		if (month == 2) {
+			days += 1;
+		}
 	}
-	else if (isLeapYear(year) == false) {
-		cout << year << " is not a leap year.";
-	}
+	//else if (isLeapYear(year) == false) {
+	//	cout << year << " is not a leap year.";
+	//}
+
+	const string MONTHS[] = { "blank", "January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
+	string month_word = MONTHS[month];
+
+	cout << month_word << " " << year << " has " << days << " days.";
+	
 
 }
 
 //funcitons
+
+int daysInMonth(int month, int year) {
+	const int MONTHDAYS[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	int month_fix = month - 1;
+	int days = MONTHDAYS[month_fix];
+	//if (isLeapYear(month) == true) {
+	//	days = days + 1;
+	//	return days;
+	//}
+	//else { return days; }
+	return days;
+}
 
 //isLeapYear - determines whether a given year is a leap year
 //@param year - the year, expected to be >= 1582
